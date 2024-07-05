2d0a8ff473cd871ee78791fbb39b919da8188f80
sejun.kim <sj.kim@greendote.co.kr>
2024-07-05 18:33:44 +0900

    feat. login page ui layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import FormInput from "@/compoennts/form-input";
+import FormButton from "@/components/form-button";
+import FormInput from "@/components/form-input";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-slate-800 text-white min-h-screen py-10">
-      <div className="flex flex-col items-center gap-2">
-      <span className="text-8xl">🍆</span>
-      <FormInput type='email' placeholder="Email" required errors={['']} />
-      <FormInput type='text' placeholder="Username"  required errors={[]}/>
-      <FormInput type='passwords' placeholder="Password" required errors={[]} />
-      <button className="w-full rounded-md bg-violet-500">Login</button>
+    <main className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-6 items-center">
+        <span className="text-8xl">🍆</span>
+        <FormInput type='email' placeholder="Email" required errors={[]} />
+        <FormInput type='text' placeholder="Username"  required errors={[]}/>
+        <FormInput type='passwords' placeholder="Password" required errors={[]} />
+        <FormButton />
       </div>
     </main>
   );
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -10,7 +10,7 @@ function FormInput(props: Props) {
 
     return (
         <>
-            <input type={type} placeholder={placeholder} required={required} className="w-full ring-2 rounded-md"/>
+            <input type={type} placeholder={placeholder} required={required} className="w-full h-10 ring-2 rounded-md px-2 focus:outline-none focus:ring-8 focus:ring-violet-300 text-gray-700"/>
             {errors.map((error, index) => (<span key={index} className="text-red-500 font-large">{error}</span>))} 
         </>
     )
